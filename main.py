700f379d02e121e575114171f26ad8a8cdac5a5c
Doobligation <jingyo899@gmail.com>
2024-03-11 17:06:09 +0900

    updated to produce txt file and a jpg file!

--- main.py
+++ main.py
@@ -1,39 +1,77 @@
 import cv2
 import argparse
+from PIL import Image, ImageDraw, ImageFont
 
 
-def to_ascii(image, chars):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.resize(image, (width, length))
-    ascii_frame = ""
-
-    for row in image:
-        for pixel_value in row:
-            index = int(pixel_value / 256 * len(chars))
-            ascii_frame += chars[index]
-        ascii_frame += "\n"
-
-    return ascii_frame
-
-
-if __name__ == "__main__":
+def parse_args():
     parser = argparse.ArgumentParser(description="Super Duper Great Cool ASCII art maker!.")
-
     parser.add_argument("input_file", help="Path to the input file")
 
     parser.add_argument("-o", "--output_file", default="result.txt", help="Path to the output file")
     parser.add_argument("-w", "--width", type=int, default=300, help="Width of the ASCII art")
     parser.add_argument("-l", "--length", type=int, default=300, help="Height of the ASCII art")
     parser.add_argument("-c", "--chars", type=str, default="@#$%/&*()_+!", help="Characters to make the ASCII art")
+    parser.add_argument("-s", "--font_size", type=int, default=1, help="Size of the font")
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
     img_path = args.input_file
-    img = cv2.imread(img_path)
+    file = cv2.imread(img_path)
+
     output_file = args.output_file
     width = args.width
     length = args.length
     chars = args.chars
+    size = args.font_size
 
-    with open(output_file, 'w') as file:
-        res = to_ascii(img, chars)
-        file.write(res)
+    to_ascii(file, output_file, chars, width, length,size)
+
+
+def to_ascii(file, output, chars, width, length, size):
+    file = cv2.cvtColor(file, cv2.COLOR_BGR2GRAY)
+    file = cv2.resize(file, (width, length))
+    ascii_frame = ""
+
+    for row in file:
+        for pixel_value in row:
+            index = int(pixel_value / 256 * len(chars))
+            ascii_frame += chars[index]
+        ascii_frame += "\n"
+
+    if str(output).split(".")[-1] == "jpg":
+        return to_jpg(ascii_frame, output, width, length, size)
+    elif str(output).split(".")[-1] == "txt":
+        with open(output, 'w') as file:
+            file.write(ascii_frame)
+
+
+def to_jpg(txt, output, width, length, size):
+    im = Image.new("1", (width, length), color="white")
+    im.convert("L")
+
+    draw = ImageDraw.Draw(im)
+
+    x, y = 0, 0
+    font_size = size
+    font = ImageFont.truetype("arial.ttf", font_size)
+    char_width = font_size
+    char_height = font_size
+
+    for char in txt:
+        draw.text((x, y), char, fill='black', font=font)
+        x += char_width
+        if x + char_width > width + 1:
+            x = 0
+            y += char_height
+
+    im.save(output)
+
+
+if __name__ == "__main__":
+    main()
+
+
